f6e6ba7f953006fc558c1308d82ef10b1545131d
ross hytnen <rossh@alleninstitute.org>
2022-05-25 22:33:57 +0000

    Mvr development

--- dynamic_routing/test_mvr.py
+++ dynamic_routing/test_mvr.py
@@ -1,33 +1,6 @@
-import threading
-
-from mvr import MVRConnector
-from pprint import pprint
-
-read_mvr = True
+'field': pre_insertion_image
+'type': high res image
+'desc': Pre-insertion photo doc
 
 
-def mvr_thread(mvr):
-    while read_mvr:
-        for message in mvr.read():
-            pprint(message)
-
-def main():
-    con = {'host': 'W10SV108131',
-           'port': 50000}
-    mvr = MVRConnector(con)
-
-    t = threading.Thread(target=mvr_thread, args=(mvr,), daemon=True)
-    t.start()
-
-    global read_mvr
-
-    while x := input('>>> '):
-        if x == 's':
-            mvr.take_snapshot()
-        elif x == 'q':
-            read_mvr = False
-            t.join()
-            break
-
-if __name__ == '__main__':
-    main()
+Given [image_path; high res image] Example image; min_size=512; max_size=512; im_rotation=0; fliplr=False; flipud=False  
