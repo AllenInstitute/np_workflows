b75af0a624f56b7558fe58802833df99885d0fb8
bjhardcastle <ben.hardcastle@alleninstitute.org>
2022-06-13 15:10:33 -0700

    Update ephys_api w two classes: http & pb

--- dynamic_routing/ephys_api.py
+++ dynamic_routing/ephys_api.py
@@ -1,99 +1,132 @@
-import time
-import requests
 import json
 
-class ephys:
-    
-    # TODO check response.status_code == 200 within object
-    
-    def __init__(self, host=None):
-        if host is None:
-            self.host = "W10SV108131" # btTest
-        else:
-            self.host = host
-            
-        self.http_server = f"http://{self.host}:37497/api/"
-        
-        # content discovered so far:
-        self.http_status = self.http_server + "status"
-        self.http_recording = self.http_server + "recording"
-        self.http_processors = self.http_server + "processors"
+from enum import Enum
 
-    def get_mode(self):
-        
-        print(f"request: 'mode' <-- {self.http_status}")
-        return requests.get(self.http_status).json()['mode']
+import logging
+
+import requests
+import time
+from . import ephys_edi_pb2 as ephys_messages
+from typing import Optional
+
+class EphysRouter:
+    @staticmethod
+    def start_ecephys_recording():
+        return ephys_messages.recording(command=1)
+
+    @staticmethod
+    def stop_ecephys_recording():
+        return ephys_messages.recording(command=0)
+
+    @staticmethod
+    def start_ecephys_acquisition():
+        return ephys_messages.acquisition(command=1)
+
+    @staticmethod
+    def stop_ecephys_acquisition():
+        return ephys_messages.acquisition(command=0)
+
+    @staticmethod
+    def set_open_ephys_name(path):
+        return ephys_messages.set_data_file_path(path=path)
+
+    @staticmethod
+    def clear_open_ephys_name():
+        return ephys_messages.set_data_file_path(path='')
+
+    @staticmethod
+    def request_open_ephys_status():
+        return ephys_messages.request_system_status(path='')
+
+    @staticmethod
+    def reset_open_ephys(io):
+        io.write(EphysRouter.clear_open_ephys_name())
+        time.sleep(.5)
+        io.write(EphysRouter.start_ecephys_recording())
+        time.sleep(3)
+        io.write(EphysRouter.stop_ecephys_recording())
+        time.sleep(.5)
 
 
-    def set_mode(self, msg: str):
-        
-        modes = ["IDLE","ACQUIRE","RECORD"]
-        
-        if msg not in modes:
-            raise ValueError(f"{msg=}: must be one of {modes=}")
-        
-        else:
-            mode_msg = bytes('{"mode":"' + msg + '"}', 'utf-8')           
-            
-            print(f"sending: {mode_msg} --> {self.http_status}")
-            return requests.put(self.http_status, mode_msg)
-        
-        
-    def get_data_file_path(self):
+class EphysHTTP:
+    server = f"http://localhost:37497/api"
+    status_endpoint = f"{server}/status"
+    recording_endpoint = f"{server}/recording"
+    processors_endpoint = f"{server}/processors"
+
+    class EphysModes(Enum):
+        idle = "IDLE"
+        acquire = "ACQUIRE"
+        record = "RECORD"
+
+    @staticmethod
+    def set_URL(url):
+        EphysHTTP.server = url
+        EphysHTTP.status_endpoint = f"{EphysHTTP.server}/status"
+        EphysHTTP.recording_endpoint = f"{EphysHTTP.server}/recording"
+        EphysHTTP.processors_endpoint = f"{EphysHTTP.server}/processors"
+
+    @staticmethod
+    def get_mode() -> requests.Response:
+        logging.info(f"request: 'mode' <-- {EphysHTTP.status_endpoint}")
+        return requests.get(EphysHTTP.status_endpoint).json()['mode']
+
+    @staticmethod
+    def set_mode(mode: EphysModes) -> requests.Response:
+        if not isinstance(mode, EphysHTTP.EphysModes):
+            raise TypeError(f"Expected mode of type EphysModes but found {type(mode)}")
+        mode_msg = bytes('{"mode":"{0}"}'.format(mode.value), 'utf-8')
+        logging.info(f"sending: {mode_msg} --> {EphysHTTP.status_endpoint}")
+        return requests.put(EphysHTTP.status_endpoint, mode_msg)
+
+    @staticmethod
+    def get_data_file_path():
         # TODO update to get folder name / directory / append as reqd
-        return requests.get(self.http_recording).json()['current_directory_name']
-        
-        
-    def set_data_file_path(self,path):
-        
-        recording = requests.get(self.http_recording).json()
-        
-        
-        sessionID = '0'*10 + "_"
-        mouseID = "_" + "366122" 
-        recording.update({'prepend_text':sessionID})
-        recording.update({'append_text':mouseID})
-     
-        # TODO update to set folder name / parent directory as reqd
-        #! folder name only seems to update when text field is clicked in gui:
-        #! does not take effect when starting a new recording
-        recording.update({'current_directory_name':path})
-        
-        return requests.put(self.http_recording, json.dumps(recording))
-    
-    
-def start_ecephys_recording():
-    return ephys().set_mode("RECORD")
+        return requests.get(EphysHTTP.recording_endpoint).json()['current_directory_name']
 
-def stop_ecephys_recording():
-    return ephys().set_mode("ACQUIRE")
+    @staticmethod
+    def set_data_file_path(path:str, prepend_text: Optional[str] = None, append_text: Optional[str] = None):
+        recording = requests.get(EphysHTTP.recording_endpoint).json()
+        if prepend_text:
+            recording['prepend_text'] = prepend_text
+        if append_text:
+            recording['append_text'] = append_text
+        recording['current_directory_name'] = path
+        return requests.put(EphysHTTP.recording_endpoint, json.dumps(recording))
 
-def start_ecephys_acquisition():
-    return ephys().set_mode("ACQUIRE")
+    @staticmethod
+    def start_ecephys_recording():
+        return EphysHTTP.set_mode(EphysHTTP.EphysModes.record)
 
-def stop_ecephys_acquisition():
-    return ephys().set_mode("IDLE")
+    @staticmethod
+    def stop_ecephys_recording():
+        return EphysHTTP.set_mode(EphysHTTP.EphysModes.acquire)
 
-def set_open_ephys_name(path):
-    return ephys().set_data_file_path(path=path)
+    @staticmethod
+    def start_ecephys_acquisition():
+        return EphysHTTP.set_mode(EphysHTTP.EphysModes.acquire)
 
-def clear_open_ephys_name():
-    return ephys().set_data_file_path(path='')
+    @staticmethod
+    def stop_ecephys_acquisition():
+        return EphysHTTP.set_mode(EphysHTTP.EphysModes.idle)
 
-def request_open_ephys_status():
-    return ephys().get_mode()
+    @staticmethod
+    def set_open_ephys_name(path: str,  prepend_text: Optional[str] = None, append_text: Optional[str] = None):
+        return EphysHTTP.set_data_file_path(path=path, prepend_text=prepend_text, append_text=append_text)
 
-def reset_open_ephys():
-    clear_open_ephys_name()
-    time.sleep(.5)
-    start_ecephys_acquisition()
-    time.sleep(3)
-    stop_ecephys_recording()
-    time.sleep(.5)
-        
+    @staticmethod
+    def clear_open_ephys_name():
+        return EphysHTTP.set_data_file_path(path='')
 
+    @staticmethod
+    def request_open_ephys_status():
+        return EphysHTTP.get_mode()
 
-set_open_ephys_name("sessionID_yyyymmdd_mouseID")
-start_ecephys_recording()
-
-reset_open_ephys()
+    @staticmethod
+    def reset_open_ephys():
+        EphysHTTP.clear_open_ephys_name()
+        time.sleep(.5)
+        EphysHTTP.start_ecephys_acquisition()
+        time.sleep(3)
+        EphysHTTP.stop_ecephys_recording()
+        time.sleep(.5)
