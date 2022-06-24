814bef0d0f2428b026717a1428e0690c43c48dd4
corbennett <corbennett@gmail.com>
2022-06-24 00:25:56 -0700

    Add tested batch files for launching

--- np/test_mvr.py
+++
@@ -1,6 +0,0 @@
-'field': pre_insertion_image
-'type': high res image
-'desc': Pre-insertion photo doc
-
-
-Given [image_path; high res image] Example image; min_size=512; max_size=512; im_rotation=0; fliplr=False; flipud=False  
