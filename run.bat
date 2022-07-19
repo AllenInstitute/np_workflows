eed2418644448d6cf32ebb05abd239cc2ed9842c
Ben <ben.hardcastle@alleninstitute.org>
2022-07-18 17:39:09 -0700

    Update run to pull

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-CALL install_excl_dyn_rout_wfl.bat
-@REM CALL install_no_pull_wfl.bat
+@REM CALL install_excl_dyn_rout_wfl.bat
+CALL install_with_pull_wfl.bat
 CALL run_router.bat
 CALL run_workflow.bat
 
