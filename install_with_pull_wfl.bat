0bb892cdc0bfef6136db01a0aaa8fa1624b01af4
Ben <ben.hardcastle@alleninstitute.org>
2022-07-21 11:53:17 -0700

    Update run.bat for multiple rigs and branches

--- install_with_pull_wfl.bat
+++ install_with_pull_wfl.bat
@@ -1,5 +1,14 @@
-git checkout np2
-git pull origin np2
+:: we're using slightly different workflow code depending on which rig we're on
+:: code for each rig is on a branch in a github repo
+
+SET rig=%AIBS_RIG_ID%
+SET branch=np
+
+IF %rig%==NP.2 SET branch=np2
+IF %rig%==NP.1 SET branch=np
+
+git checkout %branch%
+git pull origin %branch%
 
 robocopy .\np\ c:\progra~1\AIBS_MPE\workflow_launcher\np\ /MIR /E
 
@@ -7,7 +16,8 @@ set sourcefolder=.\np\workflows\
 set destfolder=c:\ProgramData\AIBS_MPE\wfltk\workflows\
 
 del %destfolder%\*.wfl
+IF %rig%==NP.2 SET AIBS_WSE_WFLS=c:\progra~1\AIBS_MPE\workflow_launcher\np\workflows\passive\
 
-for /f %%d in ('dir %sourcefolder% /b /ad ') do (
-    robocopy %sourcefolder%\%%d %destfolder% *.wfl /s /xf dynamic_routing.wfl 
-)
+@REM for /f %%d in ('dir %sourcefolder% /b /ad ') do (
+@REM     robocopy %sourcefolder%\%%d %destfolder% *.wfl /s /xf dynamic_routing.wfl 
+@REM )
