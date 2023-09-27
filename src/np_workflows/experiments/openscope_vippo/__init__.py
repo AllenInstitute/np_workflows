9883d442229ba17907f7ab20d183e8f584e15f45
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-27 13:35:04 -0700

    Fix folder names, move ipynb to `np_notebooks`

--- src/np_workflows/experiments/openscope_vippo/__init__.py
+++ src/np_workflows/experiments/openscope_vippo/__init__.py
@@ -1,2 +1,2 @@
 from .main_vippo_pilot import new_experiment, Hab, Ephys, validate_selected_workflow
-from .vippo_workflow_widget import barcode_workflow_widget
+from .vippo_workflow_widget import vippo_workflow_widget
